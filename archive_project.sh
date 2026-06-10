524286eae1792c53411554ebf4ad2f1219c6c79f
Ubuntu <ubuntu@ip-172-31-33-225.ap-south-1.compute.internal>
2026-06-10 12:15:01 +0000

    Auto backup 2026-06-10 12:15:01

--- archive_project.sh
+++ archive_project.sh
@@ -23,7 +23,7 @@ then
 fi
 
 #Find the list of files larger than 2MB
-for i in `find $BASH -maxdepth $DEPTH -type f -size +2M`
+for i in `find $BASH -maxdepth $DEPTH -type f -size +2K`
 do
 	if [ $RUN -eq 0 ]
 	then
