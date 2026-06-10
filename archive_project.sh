884a96d9336c616179c121b46735d0c65514c441
Ubuntu <ubuntu@ip-172-31-33-225.ap-south-1.compute.internal>
2026-06-10 12:00:01 +0000

    Auto backup 2026-06-10 12:00:01

--- archive_project.sh
+++ archive_project.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 #$Revision:001$
-#$
+#$Wed Jun 10 11:27:14 UTC 2026
+
+#Variables
+BASH=/home/ubuntu/backup
+DAYS=30000000
+DEPTH=1
+RUN=0
+
+#Check if the directory is present or not
+
+if [ ! -d $BASH ]
+then
+	echo "directory dose not exist: $BASH"
+	exit 1
+fi
+
+#Create 'archive' folder if not present
+if [ ! -d $BASH/archive ]
+then
+	madir $BASH/archive
+fi
+
+#Find the list of files larger than 2MB
+for i in `find $BASH -maxdepth $DEPTH -type f -size +2MB`
+do
+	if [ $RUN -eq 0 ]
+	then
+		echo "[$(date "+%Y-%m-%d %H:%M:%S")] archiving $i ==> $BASH/archive"
+		grip $i || exit 1
+		mv $i.gz $BASH/archive || exit 1
+	fi
+done
+
