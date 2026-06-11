a7ee23e3d28d895542972a275438a04e5690cd81
Ubuntu <ubuntu@ip-172-31-33-225.ap-south-1.compute.internal>
2026-06-11 12:20:01 +0000

    Auto backup 2026-06-11 12:20:01

--- user_create.sh
+++ user_create.sh
@@ -18,4 +18,49 @@ then
 fi
 
 #Store 1st argument as user name
+USER_NAME="${1}"
 
+#In case of more than one argument, store it as account comments
+
+COMMENT="${@}"
+
+#Create a password
+
+PASSWORD=$(date +%s%N)
+echo $PASSWORD
+
+#Create the user 
+useradd -c $COMMENT -m $USER_NAME
+
+#Check if user is successfully created or not
+
+if [[ $? -ne 0 ]]
+then
+	echo 'The Account could not be created'
+	exit 1
+fi
+
+#Set the password for the user
+
+password $PASSWORD $USER_NAME
+
+#Check if password id successfully set or not
+
+if [[ $? -ne 0 ]]
+then
+	echo 'Password could not be set'
+	exit 1
+fi
+
+#Force password change on first login 
+
+passwd -e $USER_NAME
+
+#Display the username, password, and the host where the user was created
+
+echo
+echo "Username: $USER_NAME"
+echo
+echo "Password: $PASSWORD"
+echo
+echo $(hostname)
